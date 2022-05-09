0e84a07151c60a8959778e2136d7f1464cc45ed3
Andrey  Airson <andrey@vallorisolutions.com>
2022-05-09 05:01:56 -0300

    feature: finished Countdown

--- module/rCount.js
+++ module/rCount.js
@@ -1,34 +1,25 @@
 const initRegressiveCount = () => {
-  // const days = document.querySelector(".days")
-  const dhour = document.querySelector(".hours")
-  const dmin = document.querySelector(".minutes")
-  const dsec = document.querySelector(".seconds")
+  const day = document.querySelector(".days");
+  const dhour = document.querySelector(".hours");
+  const dmin = document.querySelector(".minutes");
+  const dsec = document.querySelector(".seconds");
 
-  // const date = new Date("2022-05-17T11:35:00")
-  // const hour = new Date().getHours()
-  // const minute = new Date().getMinutes()
-  // const second = new Date().getSeconds()
-  // dhour.innerHTML = hour
-  // dmin.innerHTML = minute
-  // dsec.innerHTML = second
-  // console.log(date)
-  const startTime = (duration,display) =>{
-    let timer = duration, minutes, seconds;
-    setInterval(() =>{
-      hours = parseInt(timer / 3600, 10)
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
-      hours = hours < 10 ? "0" + hours : hours;
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+  const countDown = () => {
+    const now = new Date();
+    const eventDate = new Date("2022-05-17T24:00:00");
+    const diff = eventDate - now;
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-      display.textContent = `${hours}:${minutes}:${seconds}`;
-
-      if (--timer < 0) {
-        timer = duration;
-      }
-    },1000)
-  }
+    day.innerHTML = days;
+    dhour.innerHTML = String(hours).padStart(2, "0");
+    dmin.innerHTML = String(minutes).padStart(2, "0");
+    dsec.innerHTML = String(seconds).padStart(2, "0");
+  };
+  countDown();
+  setInterval(countDown, 1000);
 };
 
 export default initRegressiveCount;
